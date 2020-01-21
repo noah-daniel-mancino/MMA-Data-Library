3e4a2ad0cd20ca84f93200d341eb801e68fbbfdf
Noah Mancino <noahdanielmancino@gmail.com>
2020-01-20 21:46:57 -0800

    Scraper now iterates through each fighter's page

--- UFCFighterScraper/UFCFighterScraper/spiders/UFCFighterScraper.py
+++ UFCFighterScraper/UFCFighterScraper/spiders/UFCFighterScraper.py
@@ -1,14 +1,21 @@
 import scrapy
+import string
+
 
 class UFCFighterSpider(scrapy.spider):
     name = "UFCFighter"
 
-    start_urls = ["http://www.ufcstats.com/statistics/fighters"]
+    # The pages are alphabetized by last name.
+    start_urls = ["http://www.ufcstats.com/statistics/fighters?" +
+                  f"char={c}&page=all" for c in string.ascii_lowercase]
 
     def parse(self, response):
-        char = getattr(response.url, "char", None)
-        page = getattr(response.url, "page", None)
         table_rows = response.xpath("""//table[@class='b-statistics__table']
                                     //tbody//tr""")
-        for row in table_rows[2:]:
-            fight_page = row.xpath('td/a/@href').extract_first
+        if table_rows is not None: # I heard Rubens Xavier might get cut...
+            for row in table_rows[2:]:
+                fighter_page = row.xpath('td/a/@href').extract_first()
+                yield response.follow(fighter_page, callback=parse_fighter_page)
+
+    def parse_fighter_page(self, response):
+        pass
